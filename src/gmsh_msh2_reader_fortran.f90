468c5e35d7683afdb09cf967d171ac2872e7134b
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-19 07:15:25 +0900

    feat: add implementation: gmsh_msh2_data_type

---
+++ src/gmsh_msh2_reader_fortran.f90
@@ -0,0 +1,20 @@
+!> Version: experimental
+!> Fortran module for reading [Gmsh MSH file format version 2](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format-version-2-_0028Legacy_0029)
+module gmsh_msh2_reader
+
+    implicit none
+
+
+
+    private
+
+    public :: gmsh_msh2_data_type
+
+
+
+    !> Version: experimental
+    !> Derived type for reading [Gmsh MSH file format version 2](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format-version-2-_0028Legacy_0029)
+    type :: gmsh_msh2_data_type
+    end type gmsh_msh2_data_type
+
+end module gmsh_msh2_reader
