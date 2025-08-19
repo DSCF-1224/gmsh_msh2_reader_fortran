b2391ba6cee01af8937abf6028d64a179aaa93b8
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-20 07:13:50 +0900

    docs: update description about this fortran module
    - Add `alias` `GmshReferenceManualMshFileFormat2` on ford.md
    - Apply the introduced alias to the source file

--- src/gmsh_msh2_reader_fortran.f90
+++ src/gmsh_msh2_reader_fortran.f90
@@ -1,5 +1,5 @@
 !> Version: experimental
-!> Fortran module for reading [Gmsh MSH file format version 2](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format-version-2-_0028Legacy_0029)
+!> Fortran module for reading |GmshReferenceManualMshFileFormat2|
 module gmsh_msh2_reader
 
     implicit none
@@ -13,7 +13,7 @@ module gmsh_msh2_reader
 
 
     !> Version: experimental
-    !> Derived type for reading [Gmsh MSH file format version 2](https://gmsh.info/doc/texinfo/gmsh.html#MSH-file-format-version-2-_0028Legacy_0029)
+    !> Derived type for reading |GmshReferenceManualMshFileFormat2|
     type :: gmsh_msh2_data_type
     end type gmsh_msh2_data_type
 
