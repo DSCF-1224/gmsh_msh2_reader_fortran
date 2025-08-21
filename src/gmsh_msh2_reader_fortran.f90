64e53ab82acb3c92de2eb7f523422e9db2faf2ff
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-08-22 06:36:07 +0900

    feat!: import gmsh_msh2_data_type from gmsh_msh_format_fortran
    - Import designated derived type: gmsh_msh2_data_type for reading `$MeshFormat` ~ `$EndMeshFormat`
    - Update comments/documentation about this module

--- src/gmsh_msh2_reader_fortran.f90
+++ src/gmsh_msh2_reader_fortran.f90
@@ -19,6 +19,13 @@ module gmsh_msh2_reader
     !> Version: experimental
     !> Derived type for reading |GmshReferenceManualMshFileFormat2|
     type :: gmsh_msh2_data_type
+
+        private
+
+        !> Derived type for reading `$MeshFormat` ~ `$EndMeshFormat`  
+        !> [Documentation for this derived type](https://dscf-1224.github.io/gmsh_msh_format_fortran/type/gmsh_msh_format_type.html)
+        type(gmsh_msh_format_type) :: msh_format
+
     end type gmsh_msh2_data_type
 
 end module gmsh_msh2_reader
