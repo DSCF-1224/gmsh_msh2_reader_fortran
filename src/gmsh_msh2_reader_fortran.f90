8c0f7919d4a663aae8e8a915dd020e5bf4d2501a
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2026-03-18 06:57:54 +0900

    feat: add new subroutine gmsh_msh2_read_file

--- src/gmsh_msh2_reader_fortran.f90
+++ src/gmsh_msh2_reader_fortran.f90
@@ -26,6 +26,58 @@ module gmsh_msh2_reader
         !> [Documentation for this derived type](https://dscf-1224.github.io/gmsh_msh_format_fortran/type/gmsh_msh_format_type.html)
         type(gmsh_msh_format_type) :: msh_format
 
+
+
+        contains
+
+
+
+        procedure, nopass, private :: gmsh_msh2_read_file
+
+
+
+        generic, public :: read_file => gmsh_msh2_read_file
+
     end type gmsh_msh2_data_type
+
+
+
+    contains
+
+
+
+    subroutine gmsh_msh2_read_file(file, iostat, iomsg)
+        !! Read |GmshReferenceManualMshFileFormat2| file
+
+        character(*), intent(in) :: file
+
+        integer, intent(out) :: iostat
+
+        character(*), intent(inout) :: iomsg
+
+
+
+        integer :: file_unit
+
+
+
+        open( &!
+            newunit = file_unit , &!
+            file    = file      , &!
+            action  = 'read'    , &!
+            status  = 'old'     , &!
+            iostat  = iostat    , &!
+            iomsg   = iomsg(:)    &!
+        )
+
+        if (iostat .ne. 0) return
+
+        close( &!
+            unit = file_unit   , &!
+            iostat  = iostat   , &!
+            iomsg   = iomsg(:)   &!
+        )
+
+    end subroutine gmsh_msh2_read_file
 
 end module gmsh_msh2_reader
